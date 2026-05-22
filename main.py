cf66d0e7cb3b6a2af898da6d4ed19dabee17649b
Epic-Aaron <peteraaron004@gmail.com>
2026-05-22 20:54:41 +0100

    fix render database creation

--- main.py
+++ main.py
@@ -258,9 +258,9 @@ def delete_post(post_id):
     db.session.commit()
     return redirect(url_for('get_all_posts'))
 
-if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
+with app.app_context():
+    db.create_all()
 
+if __name__ == "__main__":
     app.run(debug=True)
 
